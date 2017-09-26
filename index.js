9dc466e89c4b706ec40cddedc079c5c4b6a24e47
james willett <jms.wlltt@gmail.com>
2017-09-26 17:12:59 -0400

    ready 2 deploy

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ const jsonParser = bodyParser.json();
 
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'James',
-  host: 'localhost',
-  database: 'James'
+const connectionString = process.env.DATABASE_URL || 'postgresql://James:@localhose:5432/James'
+
+const pool = new Pool ({
+  connectionString: connectionString
 })
 
 app.set('view engine', 'ejs')
 app.set('views', `${__dirname}/views/`)
 
 app.use(express.static('public'));
-
-
 
 app.get('/', (req, res) => {
   res.render('index');
